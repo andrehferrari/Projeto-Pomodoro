79386ea2f304b14693871f0229e2be6d92f7d4d7
André Ferrari <andrhenrique6488@gmail.com>
2022-08-25 13:33:51 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict"
+
+var minutes = 0;
+var seconds = 2;
+var countdown;
+
+var preventSpeedStimer = false;
+
+function start(){
+    if(!preventSpeedStimer){
+        preventSpeedStimer = true;
+        countdown = setInterval(timer, 1000)
+    }
+};
+
+function pause(){
+    preventSpeedStimer = false;
+    clearInterval(countdown);
+};
+
+function restart(){
+    preventSpeedStimer = false;
+    clearInterval(countdown);
+    minutes = 24;
+    seconds = 60;
+    document.getElementById('counter').innerText = '25:00'
+};
+
+function timer() {
+    seconds--;
+
+    if(seconds == 0){
+        minutes--;
+        seconds = 60;
+
+        if(minutes < 0){
+            restart();
+            document.getElementById('counter').innerText = '25:00'
+        };
+    };
+
+    var format = 
+    `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+    document.getElementById('counter').innerText = format;
+};
